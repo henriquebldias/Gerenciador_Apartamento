ea762530c82eaceef2747cc0c4f712c188e29dac
Henrique Dias <106398961+henriquebldias@users.noreply.github.com>
2025-03-20 20:22:49 -0300

    Delete src directory

--- src/main/java/com/ecxus/hotel/repository/ApartamentoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ecxus.hotel.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ecxus.hotel.entity.ApartamentoEntity;
-
-public interface ApartamentoRepository extends JpaRepository<ApartamentoEntity, String> {	
-}
